7de6e1c1c952b013b06bc5cd928c157e27c38159
seveibar <seveibar@gmail.com>
2020-11-27 02:01:38 -0500

    add empty global window object

--- tests/test-package/index.js
+++ tests/test-package/index.js
@@ -4,5 +4,6 @@ import React from "react"
 
 // <Component a={1} b={2} /> => <div>3</div>
 module.exports = ({ a, b }) => {
+  window.a = 1
   return React.createElement("div", {}, add([a, b]))
 }
--- tests/test-package/test.js
+++ tests/test-package/test.js
@@ -1,6 +1,8 @@
 import TestRenderer from "react-test-renderer"
 import TestComponent from "./index.js"
 
+global.window = {}
+
 const testRenderer = TestRenderer.create(TestComponent({ a: 1, b: 2 }))
 
 console.log(testRenderer.toJSON())
